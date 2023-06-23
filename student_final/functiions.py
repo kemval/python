3e6ba1eef52dde5fd9ae76f019ab055fbcc8b5f6
kemval <kemval@icloud.com>
2023-06-23 01:55:13 -0600

    student final code updated

--- student_final/functiions.py
+++ student_final/functiions.py
@@ -1,83 +1,26 @@
 import random
-import string
-import sys
 
-#Generates a random name
+def fill_grades(dictionary):
+    dictionary["name"] = input("Enter student's name: ")
+    dictionary["homework"] = [random.randint(0, 100) for _ in range(4)]
+    dictionary["quizzes"] = [random.randint(0, 100) for _ in range(3)]
+    dictionary["tests"] = [random.randint(0, 100) for _ in range(2)]
 
-VOWELS = "aeiou"
-CONSONANTS = "".join(set(string.ascii_lowercase) - set(VOWELS))
-
-
-def generate_name(lenght):
-    word = ""
-    for i in range(length):
-        if i % 2 == 0:
-            word += random.choice(CONSONANTS)
-        else:
-            word += random.choice(VOWELS)
-    return word
-
-"""if __name__ == "__main__":
-    try:
-        count = int(sys.argv[1])
-    except:
-        count = 4
-
-    try:
-        length = int(sys.argv[2])
-    except:
-        length = 6
-
-    for i in range(count):
-        print(generate_name(length))"""
-        
-
-
-        
-
-
-#Create a list that contains the names of the students.
-
-student_names = [generate_name(length=6)]
-
-
-# Function to calculate the average of a list
 
 def average(lst):
-    return sum(lst) / len(lst)
+    return sum(lst) / len(lst) if len(lst) > 0 else 0
 
-
-# Write a function average(list) that receives a list of numbers and returns the average of that list.
 
 def get_average(dictionary):
     homework_avg = average(dictionary["homework"])
     quizzes_avg = average(dictionary["quizzes"])
     tests_avg = average(dictionary["tests"])
-    
-    # Weights
-    homework_weight = 0.1
-    quizzes_weight = 0.3
-    tests_weight = 0.6
-    
-    weighted_average = (homework_avg * homework_weight) + (quizzes_avg * quizzes_weight) + (tests_avg * tests_weight)
-    return weighted_average
 
-
-
-def get_class_average(student_list):
-    scores = []  # Empty list to store the scores
-
-    for student in student_list:
-        student_average = get_average(student)
-        scores.append(student_average)
-
-    class_average = average(scores)
-    return class_average    
-
+    weighted_avg = 0.1 * homework_avg + 0.3 * quizzes_avg + 0.6 * tests_avg
+    return weighted_avg
 
 
 def get_letter_grade(score):
-    
     if score >= 90:
         return "A"
     elif score >= 80:
@@ -88,34 +31,12 @@ def get_letter_grade(score):
         return "D"
     else:
         return "F"
-    
 
 
-
-#Create a list that contains the names of the students.
-
-student_names = [generate_name(length=6)]
-
-
-# Function to calculate the average of a list
-
-def average(lst):
-    return sum(lst) / len(lst)
-
-
-# Write a function average(list) that receives a list of numbers and returns the average of that list.
-
-def get_average(dictionary):
-    homework_avg = average(dictionary["homework"])
-    quizzes_avg = average(dictionary["quizzes"])
-    tests_avg = average(dictionary["tests"])
-    
-    # Weights
-    homework_weight = 0.1
-    quizzes_weight = 0.3
-    tests_weight = 0.6
-    
-    weighted_average = (homework_avg * homework_weight) + (quizzes_avg * quizzes_weight) + (tests_avg * tests_weight)
-    return weighted_average
-
+def get_class_average(students):
+    scores = []
+    for student in students:
+        avg = get_average(student)
+        scores.append(avg)
+    return average(scores)
 
--- student_final/main.py
+++ student_final/main.py
@@ -1,48 +1,32 @@
 import functiions
 
-# Create 3 dictionaries that contain the following keys-values
+students = []
+repeat = True
+while True:
+  
+    while repeat:
+        student_dict = {}
+        functiions.fill_grades(student_dict)
+        students.append(student_dict)
 
-"""student_list = {}
-student_dic02 = {}
-student_dic03 = {}"""
-student_list= []
+        choice = input("Do you want to enter grades for another student? (yes/no): ")
+        if choice.lower() != "yes":
+            repeat = False
 
-students_quantity = int (input("How many students are u adding? "))
-count=0
+    print("\nStudent Gradebook:\n")
+    for student in students:
+        print("Name:", student["name"])
+        print("Homeworks:", student["homework"])
+        print("Quizzes:", student["quizzes"])
+        print("Tests:", student["tests"])
+        avg = functiions.get_average(student)
+        letter_grade = functiions.get_letter_grade(avg)
+        print("Letter Grade:", letter_grade)
+        print()
 
-while students_quantity <= count:
-    count =+1
-    for student in range ():
-        
-        name = functiions.generate_name(length=6)
-        student_list.update({"name" : name})
+    class_avg = functiions.get_class_average(students)
+    print("Class Average:", class_avg)
 
-    homework_quantity = int (input("How many homework grades are u adding? "))
-    for homework in range (homework_quantity):
-        homework = random.randint (0,100)
-        homework += 1
-        student_list.update({"Homework" : homework})
-
-    quizzes_quantity = int (input("How many quizzes grades are u adding? "))    
-
-    for quizzess in range (quizzes_quantity):
-        quizzes = random.randint (0,100) 
-        quizzes += 1
-        student_list.update({"Quizzes" : quizzes})
-
-    tests_quantity = int (input("How many test grades are u adding? "))    
-
-    for tests in range (tests_quantity):
-        test = random.randint (0,100) 
-        test += 1
-        student_list.update({"Tests" : test})
-
-    print (student_list)
-print("Please enter a number from 1 to 3")    
-
-#     student_data[studentName]= subject_data
-
-# def get_class_average(list):
-#     scores = []    
-
-
+    choice = input("Do you want to create another gradebook? (yes/no): ")
+    if choice.lower() != "yes":
+        break
